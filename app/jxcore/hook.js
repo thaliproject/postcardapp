557f00748c22866e3cd1f3e588a6ea64a8413de1
David Douglas <david@deadlyfingers.com>
2015-12-09 16:45:57 +0000

    Cordova 'before_build' hook script now async using Q - fixes #93

--- app/jxcore/hook.js
+++ app/jxcore/hook.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 module.exports = function(context) {
+  var Q = context.requireCordovaModule('q');
+  var deferral = new Q.defer();
+
   var path = require('path'),
       gulp = require('gulp'),
       gulpfile = path.join(__dirname, 'gulpfile');
@@ -8,5 +11,8 @@ module.exports = function(context) {
 
   gulp.start('cordova:build').once('task_stop', function(){
     console.log(process.cwd(), 'gulp task done');
+    deferral.resolve();
   });
+
+  return deferral.promise;
 }
